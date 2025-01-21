f7adebe49fbcfb86fc84eaf4091242f00b70a9a0
ValenButtignol <valentinbuttignol@gmail.com>
2025-01-21 17:59:56 -0300

    Update main program

--- app/src/main/java/system/Main.java
+++ app/src/main/java/system/Main.java
@@ -1,10 +1,24 @@
 package system;
 
+import system.completion.RepOkCompletion;
+import system.executor.GlobalRepOkExecutor;
+import system.executor.LlmExecutor;
+import system.repokinjector.RepOkInjector;
+import system.classparser.ClassParser;
+import system.classparser.GlobalRepOkParser;
+
 public class Main {
     public static void main(String[] args) {
+
+        String filename = "app/src/main/java/specclasses/LinkedList.java";
+        String classname = "LinkedList";
+        String modelname = "Llama3.2";
+        String promptType = "GlobalRepOk";
         
-        CodeParser codeParser = new SimpleCodeParser("app/src/main/resources/repok1.txt");  
-        RepOkInjector repOkInjector = new RepOkInjector("app/src/main/resources/LinkedList.java", "LinkedList", codeParser.parse());
+        ClassParser classParser = new GlobalRepOkParser(filename, classname);    
+        LlmExecutor llmExecutor = new GlobalRepOkExecutor(classParser.parseClass(), modelname, filename, promptType);
+        RepOkCompletion repOkCompletion = llmExecutor.execute();
+        RepOkInjector repOkInjector = new RepOkInjector(filename, classname, repOkCompletion.getRepOk());
         repOkInjector.injectRepOk();
     }
 }
