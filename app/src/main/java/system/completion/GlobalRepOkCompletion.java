e5c3c9043813076a6f6eb767261608e1220d37c0
ValenButtignol <valentinbuttignol@gmail.com>
2025-01-22 17:07:12 -0300

    Make GlobalRepOkPrompt an abstract class

--- app/src/main/java/system/completion/GlobalRepOkCompletion.java
+++ app/src/main/java/system/completion/GlobalRepOkCompletion.java
@@ -2,6 +2,16 @@ package system.completion;
 
 public class GlobalRepOkCompletion implements RepOkCompletion {
     public String getRepOk() {
+        // TODO: This has to search for the first occurrence of the method repOk:
+        /* 
+         * ```java
+         * public boolean repOk() {
+         *      code
+         * }
+         * ```
+         * 
+         */
+
         return "";
     }
 }
