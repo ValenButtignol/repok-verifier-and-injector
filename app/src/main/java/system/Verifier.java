3e2789d81a0d90a6e89f111d33b75280e8f9a528
ValenButtignol <valentinbuttignol@gmail.com>
2025-02-03 16:20:14 -0300

    Add Verifier class that runs verifier script

---
+++ app/src/main/java/system/Verifier.java
@@ -0,0 +1,37 @@
+package system;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Verifier {
+
+    public Integer verify() {
+        Integer exitCode = -1;
+
+        String scriptPath = "./../scripts/verifier.sh";
+        ProcessBuilder processBuilder = new ProcessBuilder(scriptPath);
+        processBuilder.redirectErrorStream(true);
+
+        try {
+            Process process = processBuilder.start();
+
+            // Capture script output
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            String line;
+            
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                output.append(line).append("\n");
+            }
+
+            exitCode = process.waitFor();
+            System.out.println("Exit code: " + exitCode);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return exitCode;
+    }
+}
+
+
