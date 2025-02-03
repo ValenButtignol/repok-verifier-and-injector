654c35488d023c53dbbf49c9b82e70dd975941d2
ValenButtignol <valentinbuttignol@gmail.com>
2025-02-03 16:19:35 -0300

    Add log folder and automatic randoop script

---
+++ scripts/verifier.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TEST_DIR="../app/src/test/java"
+
+# 1. Delete previous test files
+echo "Cleaning test files..."
+rm -f "$TEST_DIR/ErrorTest.java" "$TEST_DIR/ErrorTest0.java" "$TEST_DIR/RegressionTest.java" "$TEST_DIR/RegressionTest0.java"
+
+# 1. Compile generated class
+echo "Compiling..."
+gradle build > ../logs/compile.log 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error: Failed compilation. Check compile.log"
+    exit -1
+fi
+
+# 2. Run Randoop
+echo "Running Randoop..."
+gradle runRandoop > ../logs/randoop.log 2>&1
+if [ $? -ne 0 ]; then
+    echo "Error: Failed randoop execution. Check randoop.log"
+    exit -1
+fi
+
+# 3. Verify generated test
+if grep -q "Error" ../logs/randoop.log; then
+    echo "Error: Error test generated. Check repOk."
+    exit 0
+else
+    echo "Success: Class have been tested succesfully."
+    exit 1
+fi
