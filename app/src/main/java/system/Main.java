34028993c947a2529a940f95ac360c13d127db15
ValenButtignol <valentinbuttignol@gmail.com>
2025-02-04 18:18:08 -0300

    Update main

--- app/src/main/java/system/Main.java
+++ app/src/main/java/system/Main.java
@@ -3,27 +3,51 @@ package system;
 import java.io.File;
 
 import system.classfixer.ClassFixer;
-import system.codeinjector.CodeInjector;
-import system.codeinjector.RepOkInjector;
-import system.codeparser.CodeParser;
+import system.codeparser.InvariantParser;
+import system.codeparser.PropertiesClassParser;
 import system.codeparser.RepOkClassParser;
+import system.invhandler.InvariantHandler;
+import system.invhandler.PropertiesHandler;
+import system.invhandler.RepOkHandler;
 
 public class Main {
     public static void main(String[] args) {
         String classPath = "src/../../LinkedList.java";
         String className = "LinkedList";
         
+
         ClassFixer classFixer = new ClassFixer(classPath, className);
-        File classFile = classFixer.generateCopy();
         classFixer.rewriteClassList();
-        //String promptType = "";
+        File classFile = classFixer.generateCopy();
         
-        CodeParser codeParser = new RepOkClassParser();
+        // InvariantHandler invariantHandler = new RepOkHandler(classFile, className);
+        // InvariantParser codeParser = new RepOkClassParser();
+        // codeParser.parse();
+        // codeParser.getInvariants().forEach(method -> {
+        //     invariantHandler.inject(method);
+        // });
+        // invariantHandler.verify();
+        // invariantHandler.deleteLastInjections();
+        // invariantHandler.buildRepOk();
+        // classFixer.copyBack();
+        // // invariantHandler.deleteLastInjections();
+        // classFixer.deleteCopy();
+        
+
+        InvariantHandler invariantHandler = new PropertiesHandler(classFile, className);
+        InvariantParser codeParser = new PropertiesClassParser();
         codeParser.parse();
-        CodeInjector repOkInjector = new RepOkInjector(classFile, className, codeParser.getMethods());
-        repOkInjector.inject();
-        Verifier verifier = new Verifier();
-        verifier.verify();
+        codeParser.getInvariants().forEach(method -> {
+            invariantHandler.inject(method);
+            invariantHandler.verify();
+            invariantHandler.deleteLastInjections();
+        });
+        invariantHandler.buildRepOk();
+        classFixer.copyBack();
+
+        System.out.println(invariantHandler.getVerifiedInvariants());
+        
+
 
         // TODO: In some way i need to add the repOk method to the original class
         
