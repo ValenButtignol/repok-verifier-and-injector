70f5d5cba574d57182b3194c4d58ce44b6103cf2
ValenButtignol <valentinbuttignol@gmail.com>
2025-02-05 16:05:32 -0300

    Add Exception throw in main

--- src/main/java/system/Main.java
+++ src/main/java/system/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         if (args.length < 3) {
             System.out.println("Arguments: <classPath> <className> <promptType>");
+            throw new IllegalArgumentException("Invalid number of arguments");
         }
 
         String classPath = args[0];
