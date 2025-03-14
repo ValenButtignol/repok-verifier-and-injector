80466131e4b3d4d3fea2ddf4c263b84f200edfb5
ValenButtignol <valentinbuttignol@gmail.com>
2025-03-14 10:50:21 -0300

    Change form of detecting error-revealing tests, and remove more regression tests

--- scripts/verifier.sh
+++ scripts/verifier.sh
@@ -4,7 +4,7 @@ TEST_DIR="src/test/java"
 
 # 1. Delete previous test files
 echo "Cleaning test files..."
-rm -f "$TEST_DIR/ErrorTest.java" "$TEST_DIR/ErrorTest0.java" "$TEST_DIR/RegressionTest.java" "$TEST_DIR/RegressionTest0.java"
+rm -f "$TEST_DIR/ErrorTest.java" "$TEST_DIR/ErrorTest0.java" "$TEST_DIR/RegressionTest.java" "$TEST_DIR/RegressionTest0.java" "$TEST_DIR/RegressionTest1.java"
 
 gradle build > logs/compile.log 2>&1
 if [ $? -ne 0 ]; then
@@ -19,8 +19,8 @@ if [ $? -ne 0 ]; then
 fi
 
 # 3. Verify generated test
-if grep -q "Error" logs/randoop.log; then
-    echo -e "\e[1;31m TESTS FAILED \e[0m: Check repOk.\n"
+if grep -q "Wrote error-revealing JUnit tests." logs/randoop.log; then
+    echo -e "\e[1;31m WROTE ERROR-REVEALING TESTS \e[0m: Check error-revealing JUnit tests.\n"
     exit 0
 else
     echo -e "\e[0;32m SUCCESS \e[0m: Class invariant verified.\n"
